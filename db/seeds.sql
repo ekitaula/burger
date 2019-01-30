4ff6083ea7115be5c1f8fcfe78ba36e75b1038b1
Eki <ekitaula@gmail.com>
2019-01-30 01:32:23 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Meat Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Field Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beast Burger', true);
